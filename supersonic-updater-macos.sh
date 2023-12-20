4adb4bad29065a2741d9f40d0e09759161b20189
GavinL2001 <51942756+GavinL2001@users.noreply.github.com>
2023-12-20 00:57:48 -0600

    Added source credit, changed comments, updated README

--- supersonic-updater-macos.sh
+++ supersonic-updater-macos.sh
@@ -1,57 +1,60 @@
 # Supersonic Update Script for macOS.
 # Created by Gavin Liddell
+# https://github.com/GavinL2001/supersonic-update-script
 
-#!/bin/bash
+#!/bin/zsh
+
+# Functions
 
 get_local_version() {
-	defaults read /Applications/Supersonic.app/Contents/Info.plist CFBundleShortVersionString												# Fetch current version installed.
+	defaults read /Applications/Supersonic.app/Contents/Info.plist CFBundleShortVersionString                                            # Fetch current version installed.
 }
 
-get_latest_version() {
-	# Credit to @lukechilds on Github for the script.
-	curl --silent "https://api.github.com/repos/dweymouth/supersonic/releases/latest" | 													# Get latest release from GitHub api
-	grep '"tag_name":' |																													# Get tag line
-	sed -E 's/.*"v([^"]+)".*/\1/'																											# Pluck JSON value
+get_latest_version() {                                                                                                                   # Credit to @lukechilds on Github for the function.
+	curl --silent "https://api.github.com/repos/dweymouth/supersonic/releases/latest" |                                                  # Get latest release from GitHub api.
+	grep '"tag_name":' |                                                                                                                 # Get tag line.
+	sed -E 's/.*"v([^"]+)".*/\1/'                                                                                                        # Pluck JSON value.
 }
 
 get_platform() {
-	uname -m | sed 's/86_//'																												# Get the processor type of the user's machine
+	uname -m | sed 's/86_//'                                                                                                             # Get the processor type of the user's machine.
 }
 
 get_kernel() {
-	uname -r | sed 's/\..*//'																												# Get macOS version number.
+	uname -r | sed 's/\..*//'                                                                                                            # Get macOS version number.
 }
 
 install() {
-		curl -sL $download_url -o $tmp																										# Grab the latest .zip file from GitHub and move to the user's tmp folder.
-		unzip -o -qq $tmp -d /Applications/																									# Unzip file into the Applications folder.
-		rm $tmp																																# Remove the downloaded zip file after unzipping.
-		[ "$(get_local_version)" == "$latest" ] && echo "Update completed!" || echo "Update failed."										# Check that the update worked as intended.
+		curl -sL $download_url -o $location                                                                                              # Grab the latest .zip file from GitHub and move to the user's tmp folder.
+		unzip -o -qq $location -d /Applications/                                                                                         # Unzip file into the Applications folder.
+		rm $location                                                                                                                     # Remove the downloaded zip file after unzipping.
+		[ "$(get_local_version)" == "$latest" ] && echo "Update completed!" || echo "Update failed."                                     # Check that the update worked as intended.
 		exit 0
 }
 
-local=$(get_local_version)																													# Sets the local version function as a variable.
-latest=$(get_latest_version)																												# Sets the latest version as a variable.
-platform=$(get_platform)																													# Set the platform version as a variable.
-kernel=$(get_kernel)																														# Set the kernel version as a variable.
-tmp=$HOME/tmp/Supersonic.zip																												# Set the download directory as a variable.
+# Variables
 
-printf "Your Supersonic Version: $local\nLatest Supersonic Version: $latest\n"																# Output current and latest version.
+local=$(get_local_version)                                                                                                               # Sets the local version function as a variable.
+latest=$(get_latest_version)                                                                                                             # Sets the latest version as a variable.
+platform=$(get_platform)                                                                                                                 # Set the platform version as a variable.
+kernel=$(get_kernel)                                                                                                                     # Set the kernel version as a variable.
+location=$HOME/tmp/Supersonic.zip                                                                                                        # Set the download directory as a variable.
 
-if [[ "$local" == "$latest" ]]																												# Compare the installed and latest version from GitHub.
+# The Update Process
 
-	then
-		echo "You are up-to-date!"																											# Explains that the user's version is already up-to-date.
-		exit 0
+printf "Supersonic Update Scrip for macOS\nCreated by Gavin Liddell\nRepo: https://github.com/GavinL2001/supersonic-update-script\n"     # Show source and creator of the project.
+sleep 1
+printf "\nChecking for update.\nYour Supersonic Version: $local\nLatest Supersonic Version: $latest\n"                                   # Output current and latest version.
 
-	elif [[ $kernel < 18 ]]																													# Checks if OS version is legacy.
+[ "$local" == "$latest" ] && echo "You are up-to-date!"	&& exit 0 ||                                                                     # Compare the installed and latest version from GitHub.
 
-	then																																	# Installs legacy version if the statement is true
-		printf "Your Supersonic version is out-of-date!\nDownloading the latest legacy update.\n" &&										# Explain that the application is out-of-date and that the update is being downloaded.
-		download_url="https://github.com/dweymouth/supersonic/releases/latest/download/Supersonic-$latest-mac-legacy-HighSierra-x64.zip"	# Set the download_url variable.
-		install																																# Call the install function.
+if [[ $kernel < 18 ]]                                                                                                                    # Checks if OS version is legacy.
+	then                                                                                                                                 # Installs legacy version if the statement is true.
+		download_url="https://github.com/dweymouth/supersonic/releases/latest/download/Supersonic-$latest-mac-legacy-HighSierra-x64.zip" # Set the download_url variable.
+		printf "Your Supersonic version is out-of-date!\nInstalling the latest legacy update.\n"                                         # Explain that the application is out-of-date and that the update is being downloaded.
+		install                                                                                                                          # Calls the install function.
 	else
-		printf "Your Supersonic version is out-of-date!\nDownloading the latest update.\n" &&												# Explain that the application is out-of-date and that the update is being downloaded.
-		download_url="https://github.com/dweymouth/supersonic/releases/latest/download/Supersonic-$latest-mac-$platform.zip"				# Set the download_url variable.
-		install																																# Call the install function.
+		download_url="https://github.com/dweymouth/supersonic/releases/latest/download/Supersonic-$latest-mac-$platform.zip"             # Set the download_url variable.		
+		printf "Your Supersonic version is out-of-date!\nInstalling the latest update.\n"                                                # Explain that the application is out-of-date and that the update is being downloaded.
+		install                                                                                                                          # Calls the install function.
 fi
